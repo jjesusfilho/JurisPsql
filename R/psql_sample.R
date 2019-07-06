584f077ee5178833967c6cc5bf6a089295781508
José de Jesus Filho <jjesusfilho@gmail.com>
2019-07-06 10:11:38 -0300

    first functions

---
+++ R/psql_sample.R
@@ -0,0 +1,27 @@
+#' Gets a table's sample
+#'
+#' @param con connection
+#' @param tbl table name
+#' @param column optional column name to apply filter
+#' @param value optional column value to apply filter
+#' @param n number of rows, defaults to 10.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+psql_sample <- function(con, tbl=NULL,column=NULL,value=NULL, n=10) {
+  if (is.null(column)){
+    query<-glue::glue_sql("SElECT *
+                      FROM {`tbl`}
+                      ORDER BY random() LIMIT {n}",
+                          .con=con)
+  } else {
+    query<-glue::glue_sql("SElECT *
+                      FROM {`tbl`}
+                      WHERE {`tbl`}.{`column`} = {value}
+                      ORDER BY random() LIMIT {n}",
+                          .con=con)
+  }
+  DBI::dbGetQuery(con,query)
+}
