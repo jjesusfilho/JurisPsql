e152210f20c6d41690ec9759642dbe77718d85b5
José de Jesus Filho <jjesusfilho@gmail.com>
2019-07-15 00:18:34 -0300

    Adds full-text search.

---
+++ R/psql_cjpg_query.R
@@ -0,0 +1,27 @@
+#' PostgreSQL full-text search
+#'
+#' @param con connection
+#' @param tbl table
+#' @param query words to be searched. Separate by "&" (AND), "|" (OR),
+#'     or "<->" (FOLLOWED BY) to search for more than one word.
+#' @param language Defaults to "portuguese"
+#'
+#' @return A data.frame with the cjpg decisions according to
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' dplyr::copy_to(con,"consumidor",consumidor)
+#' df<-psql_cjpg_query(con,"consumidor","contrato & alguel")
+#' df<-psql_cjpg_query(con,"consumidor","contrato | alguel")
+#' # Search for Julgo Procedente separated by up to 3 words:
+#' df<-psql_cjpg_query(con,"consumidor","julgo <3> procedente")
+#' }
+psql_cjpg_query <- function(con,tbl,query,language="portuguese"){
+  target<-"document_tokens"
+  q <-glue::glue_sql("SELECT processo,classe,assunto,comarca,foro,vara,disponibilizacao, julgado FROM {`tbl`}
+                        WHERE {`target`} @@ to_tsquery({language},{query})",.con=con)
+
+  df<-DBI::dbGetQuery(con,q)
+
+}
---
+++ R/psql_cjpg_tokenize.R
@@ -0,0 +1,34 @@
+#' Creates an index from columns assunto and julgados
+#'
+#' @param con connection
+#' @param tbl table
+#' @param language Defaults to "portuguese"
+#'
+#' @return No R object is returned
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(JurisPsql)
+#' con<-dbx::dbxConnect()
+#' dplyr::copy_to(con,"consumidor",consumidor)
+#' psql_cjpg_tokenize(con,"consumidor")
+#' }
+psql_cjpg_tokenize <- function(con,tbl,language="portuguese"){
+
+  source<-list(a = c("assunto", "A"), j = c("julgado", "B"))
+  target<-"document_tokens"
+  language<-"portuguese"
+  query<-glue::glue_sql("ALTER TABLE {`tbl`} ADD COLUMN {`target`} TSVECTOR",.con=con)
+
+  res<-DBI::dbSendQuery(con,query)
+  DBI::dbClearResult(res)
+
+  query<-glue::glue_sql("UPDATE {`tbl`} SET
+                         {`target`} = setweight(to_tsvector({language},coalesce({`source$a[1]`},'')),{source$a[2]}) ||
+                         setweight(to_tsvector({language},coalesce({`source$j[1]`}, '')), {source$j[2]})",.con=con)
+
+  res<-DBI::dbSendQuery(con,query)
+  DBI::dbClearResult(res)
+
+}
